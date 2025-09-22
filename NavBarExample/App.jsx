85fd51e2ef62d1259ab9623548d4227b135ed9d7
poojaseegi <poojaseegi414@gmail.com>
2025-09-22 12:27:55 +0530

    Create App.jsx

---
+++ NavBarExample/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+const App = ()=>{
+
+
+    return(
+        <>
+            <Nav/>
+            
+        </>
+    )
+}
+export default App;
