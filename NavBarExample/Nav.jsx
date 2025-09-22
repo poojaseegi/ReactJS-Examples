25df7550643a86117c59d1491c1861de9d5bd846
poojaseegi <poojaseegi414@gmail.com>
2025-09-22 12:28:24 +0530

    Create Nav.jsx

---
+++ NavBarExample/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/global.css"
+
+
+const Nav = ()=> {
+    return(
+        <>
+      <div className="nav">
+      <div className="left">
+     <h1>LOGO</h1>
+    </div>
+    <div className="right">
+        <div className="signup"><h1>SIGNUP</h1></div>
+         <div className="login"><h1>LOGIN</h1></div>
+       
+      </div>
+
+     </div>
+
+</>
+    )
+}
+
+export default Nav
